951f44df718c772b42ba938947f2e875130be51e
kuentin <nachituz@gmail.com>
2011-12-31 20:50:05 +0000

    index with requireJS

---
+++ day-06/index.php
@@ -0,0 +1,34 @@
+<?php 
+    $title = "HTML5 Practice";
+    $subtitle = "Day 6";
+?>
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title><?php echo "$title: $subtitle"; ?></title>
+        <link href="css/default.css" rel="stylesheet" />
+        
+        <!-- jQuery -->
+        <!-- include jquery toolkit here -->
+        
+        <!-- default javascript content -->
+        <!-- include your default js file here -->
+		<!-- !!ex1: add requirejs to the provided index.php!! -->
+		<script data-main="scripts/main" src="scripts/require.js"></script>
+    </head>
+    <body>
+        <header>
+            <h1><?php echo "<span class='title'>$title</span>: <span class='subtitle'>$subtitle</span>"; ?></h1>
+        </header>
+
+        <!-- add your content here -->
+        <h3>-- Do you have The Matrix in VHS? :O --</h3>
+        
+        <footer>
+            <div><?php echo $title; ?> v1.0</div>
+            <div>@author Alberto Miranda <a href="mailto:alberto@nextive.com">&lt;alberto@nextive.com&gt;</a></div>
+            <div>@author Esteban Abait <a href="mailto:esteban.abait@nextive.com">&lt;esteban.abait@nextive.com&gt;</a></div>
+        </footer>
+    </body>
+</html>
