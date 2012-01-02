551443f7a01ccff61b8d691354acf8f40d583b36
kuentin <nachituz@gmail.com>
2012-01-02 14:34:36 +0000

    ex1: underscore is a utility-belt library for JS that provides a lot of functional programming support

--- day-07/js/underscore.js
+++ day-07/js/underscore.js
@@ -5,6 +5,7 @@
 //     Oliver Steele's Functional, and John Resig's Micro-Templating.
 //     For all details and documentation:
 //     http://documentcloud.github.com/underscore
+//
 
 (function() {
 
