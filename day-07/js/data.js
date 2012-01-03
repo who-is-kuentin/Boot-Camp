7a2f637f776fd1cbdcaf6b7a7e684b8a451c6fb6
kuentin <nachituz@gmail.com>
2012-01-03 14:28:34 +0000

    ex3: create a JSON file with a profile

---
+++ day-07/js/data.js
@@ -0,0 +1,9 @@
+var jsonData = {
+	"name": "Philip J. Fry",
+	"cvitae": [
+		{"info": "98.000 Million Years Old"},
+		{"info": ["Delivery Boy at Panucci Pizza","Delivery Boy at Planet Express"]},
+		{"info": "Married w/ Turanga Leela"},
+		{"info": "Professor Farnsworth"},
+	]
+};
